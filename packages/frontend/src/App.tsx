49bf0fa9e0107544402f23f795c59cd59c96e28f
Xaes Design <designxaes@gmail.com>
2021-10-21 16:02:29 -0600

    Fixed homepage not redirecting to "Explore".

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,16 +4,19 @@ import Home from "./modules/home";
 import Album from "./modules/album";
 import Layout from "./components/layout";
 import NotFound from "./modules/not-found";
+import AlbumAdd from "./modules/album-add";
 import ColorSchemeProvider from "./context/color-scheme";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 const App: FC = () => (
     <ColorSchemeProvider>
         <Router>
             <Layout>
                 <Switch>
+                    <Route exact strict path="/" component={() => <Redirect to={Config.LINKS.EXPLORE_BY_ALBUM} />} />
                     <Route path={Config.LINKS.HOME} component={() => <Home />} />
                     <Route exact path={Config.LINKS.ALBUM} component={() => <Album />} />
+                    <Route exact path={Config.LINKS.NEW_ALBUM} component={() => <AlbumAdd />} />
                     <Route path="*" component={() => <NotFound />} />
                 </Switch>
             </Layout>
