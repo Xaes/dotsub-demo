315f04597313192758d93b611e0ac5c18c57c538
Xaes Design <designxaes@gmail.com>
2021-10-21 22:51:18 -0600

    Added SVG to list of purges.

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -14,7 +14,7 @@ const grayPallette = {
 };
 
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    purge: ["./src/**/*.{js,jsx,ts,tsx,svg}", "./public/index.html"],
     darkMode: "class",
     theme: {
         extend: {
