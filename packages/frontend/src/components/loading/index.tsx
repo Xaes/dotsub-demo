b014c429c657e48c2d0dc9a06acf9f6d06d6f029
Xaes Design <designxaes@gmail.com>
2021-10-29 16:09:33 -0600

    Fixed loading spacing issues.

--- packages/frontend/src/components/loading/index.tsx
+++ packages/frontend/src/components/loading/index.tsx
@@ -5,12 +5,20 @@ const Loading: FC<{ loading: boolean }> = ({ loading, children }) => {
     if (loading)
         return (
             <div className="relative">
-                <div className="opacity-50 select-none pointer-events-none">
-                    {children}
-                </div>
-                <div className="absolute inset-0 flex items-center justify-center p-12">
-                    <ReactComponent className="animate-spin h-8 w-8 fill-current text-primary" />
-                </div>
+                {children ? (
+                    <React.Fragment>
+                        <div className="opacity-50 select-none pointer-events-none">
+                            {children}
+                        </div>
+                        <div className="absolute inset-0 flex items-center justify-center p-12">
+                            <ReactComponent className="animate-spin h-8 w-8 fill-current text-primary" />
+                        </div>
+                    </React.Fragment>
+                ): (
+                    <div className="flex items-center justify-center p-12">
+                        <ReactComponent className="animate-spin h-8 w-8 fill-current text-primary" />
+                    </div>
+                )}
             </div>
         );
     else return <React.Fragment>{children}</React.Fragment>;
